c35c2b03316f5c64849b9e5cef349c79304485df
Marbiru <code@uribram.com>
2017-11-21 08:33:57 +0200

    init with placeValues challenge example

---
+++ placeValues.js
@@ -0,0 +1,24 @@
+function getPlaceValues(number) {
+    // this function takes a number of any length
+    // and returns an array containing its place values
+
+    var placeValues = [];
+    var numberLength = Math.floor(Math.log10(number)) + 1;
+    numberLength -= 1;
+
+    while (numberLength >= 0) {
+        // get the current place value, e.g. 10, 100, 1000 etc
+        var currentPlace = Math.pow(10,numberLength);
+        // get the current digit, e.g. 3 from 3291
+        var currentDigit = Math.floor(number/currentPlace);
+        // multiply by current place and push to array
+        placeValues.push(currentDigit*currentPlace);
+        // remove current place from number
+        number %= currentPlace;
+        // decrement number length to repeat process
+        // for next digit
+        numberLength -= 1;
+    }
+
+    return placeValues;
+}
